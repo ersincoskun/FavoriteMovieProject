598c07a9bd3a3a2f8889fd792786a6b09905d8f7
Ersin <ersincoskun@hotmail.com>
2021-03-28 20:43:23 +0300

    checking network connection added

--- app/src/main/java/com/ersincoskun/taskapp/view/SplashActivity.kt
+++ app/src/main/java/com/ersincoskun/taskapp/view/SplashActivity.kt
@@ -1,19 +1,22 @@
 package com.ersincoskun.taskapp.view
 
-import android.content.Context
 import android.content.Intent
 import android.net.*
-import android.opengl.Visibility
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import com.ersincoskun.taskapp.R
+import androidx.lifecycle.Observer
 import com.ersincoskun.taskapp.databinding.ActivitySplashActivityBinding
+import com.ersincoskun.taskapp.util.NetworkConnection
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import java.lang.Thread.sleep
 
 class SplashActivity : AppCompatActivity() {
-
 
     private lateinit var binding: ActivitySplashActivityBinding
 
@@ -21,43 +24,25 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        var connectivityInfo = false
-        val connectivityManager =
-            this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
         supportActionBar?.hide()
+        Handler().postDelayed({
+            observeConnectionData()
+        },3000)
+    }
 
-        if (Build.VERSION.SDK_INT < 29) {
-            val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
-            connectivityInfo = activeNetwork?.isConnectedOrConnecting == true
-        } else {
-            val mCallback = object : ConnectivityManager.NetworkCallback() {
-                override fun onLost(network: Network) {
-                    super.onLost(network)
-                    connectivityInfo = false
-                }
-
-                override fun onAvailable(network: Network) {
-                    super.onAvailable(network)
-                    connectivityInfo = true
-                }
+    fun observeConnectionData() {
+        val connectivityInfoLiveData = NetworkConnection(this)
+        connectivityInfoLiveData.observe(this, Observer {
+            if (it) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                binding.warningImageView.visibility = View.VISIBLE
+                binding.warningTv.visibility = View.VISIBLE
             }
-            val requestBuilder = NetworkRequest.Builder()
-                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
-                .addTransportType(NetworkCapabilities.TRANSPORT_ETHERNET)
-                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-            connectivityManager.registerNetworkCallback(requestBuilder.build(),mCallback)
-        }
-
-        if (connectivityInfo==true) {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }else{
-            binding.warningImageView.visibility=View.VISIBLE
-            binding.warningTv.visibility=View.VISIBLE
-        }
-
+        })
     }
 
 }
